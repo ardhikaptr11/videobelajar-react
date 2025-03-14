0521b6b18cf7e86bce2e3b5af8518e56a5b72a23
Ardhika <ardhikaptr11@gmail.com>
2025-03-14 13:45:28 +0700

    📦 Add a new component to replace 'ProtectedElement'.

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,33 @@
+import { lazy } from "react";
+import { Outlet } from "react-router";
+import { Flex } from "antd";
+
+const NotAuthorized = lazy(() => import("@pages/NotAuthorized/NotAuthorized"));
+
+import storeUser from "@store/storeUser";
+import HomePageLayout from "@components/Layouts/HomePageLayout";
+
+import { CustomSpinner } from "@components/CustomComponent/CustomSpinner";
+
+export const PrivateRoute = () => {
+	const currentUser = storeUser((state) => state.currentUser);
+	const isDeleting = sessionStorage.getItem("isDeleting");
+
+	if (isDeleting) {
+		return (
+			<Flex align="center" style={{ height: "100vh", justifyContent: "center" }}>
+				<CustomSpinner text="Redirecting" />
+			</Flex>
+		);
+	}
+
+	if (!currentUser && !isDeleting) {
+		return <NotAuthorized />;
+	}
+
+	return (
+		<HomePageLayout>
+			<Outlet />
+		</HomePageLayout>
+	);
+};
