f6c769af79e3fcab9f34e6e4c718d1e8c836de90
Ardhika <ardhikaptr11@gmail.com>
2025-03-14 15:10:23 +0700

    🔥 Remove unecessary comments.

--- src/store/storeNavigation.js
+++ src/store/storeNavigation.js
@@ -1,10 +1,6 @@
 import { create } from "zustand";
 
 const storeNavigation = create((set) => ({
-	// isDropdownVisible: false,
-	// toggleDropdown: () => set((state) => ({ isDropdownVisible: !state.isDropdownVisible })),
-	// showDropdown: () => set({ isDropdownVisible: true }),
-	// hideDropdown: () => set({ isDropdownVisible: false })
 	location: "",
 	setLocation: (location) => set({ location: location })
 }));
