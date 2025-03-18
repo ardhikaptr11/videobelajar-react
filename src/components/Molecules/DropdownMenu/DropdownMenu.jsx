61e5b9cad278a3f654b05dfb2cc53d9aacbd7020
Ardhika <ardhikaptr11@gmail.com>
2025-03-18 22:19:03 +0700

    🔧 Simplify login status check by directly evaluating currentUser

--- src/components/Molecules/DropdownMenu/DropdownMenu.jsx
+++ src/components/Molecules/DropdownMenu/DropdownMenu.jsx
@@ -4,8 +4,7 @@ import DropdownUser from "@components/Molecules/DropdownUser/DropdownUser";
 
 const DropdownMenu = () => {
 	const currentUser = storeUser((state) => state.currentUser);
-	const getLoginStatus = storeUser((state) => state.getLoginStatus);
-	const isLoggedIn = getLoginStatus(currentUser);
+	const isLoggedIn = !!currentUser;
 
 	return isLoggedIn ? <DropdownUser /> : <DropdownGuest />;
 };
