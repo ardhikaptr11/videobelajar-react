7b59c783ce8cc85bdb54701be94710eae61522b7
Ardhika <ardhikaptr11@gmail.com>
2025-03-18 22:27:08 +0700

    🔧 Refactor LandingPage to remove Suspense and enhance loading spinner with custom text

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,3 @@
-import { Suspense } from "react";
-import { Spin } from "antd";
-
 import Heroku from "@components/Molecules/Heroku/Heroku";
 import Heading from "@components/Molecules/Heading/Heading";
 import CourseList from "@components/Molecules/CourseList/CourseList";
@@ -11,13 +8,11 @@ import TabsWithIndicator from "@components/Molecules/TabsWithIndicator/TabsWithI
 import { CustomSpinner } from "@components/CustomComponent/CustomSpinner";
 import useFetchCourses from "../../customHooks/useFetchCourses";
 
-
 const LandingPage = () => {
 	const { courses, loading } = useFetchCourses();
 
-
 	return (
-		<Suspense fallback={<Spin />}>
+		<>
 			<Heroku
 				tagline="Revolusi Pembelajaran: Temukan Ilmu Baru melalui Platform Video Interaktif!"
 				description="Temukan ilmu baru yang menarik dan mendalam melalui koleksi video pembelajaran berkualitas tinggi.Tidak hanya itu, Anda juga dapat berpartisipasi dalam latihan interaktif yang akan meningkatkan pemahaman Anda."
@@ -30,13 +25,13 @@ const LandingPage = () => {
 					childText="Jelajahi Dunia Pengetahuan Melalui Pilihan Kami!"
 				/>
 				<TabsWithIndicator categories={["Semua Kelas", "Pemasaran", "Desain", "Pengembangan Diri", "Bisnis"]} />
-				{loading ? <CustomSpinner /> : <CourseList courses={courses} />}
+				{loading ? <CustomSpinner text="Loading" /> : <CourseList courses={courses} />}
 			</Course>
 			<Newsletter
 				headingText="Mau Belajar Lebih Banyak?"
 				childText="Temukan ilmu baru yang menarik dan mendalam melalui koleksi video pembelajaran berkualitas tinggi. Tidak hanya itu, Anda juga dapat berpartisipasi dalam latihan interaktif yang akan meningkatkan pemahaman Anda."
 			/>
-		</Suspense>
+		</>
 	);
 };
 
